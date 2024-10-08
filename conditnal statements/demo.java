6fd6b9562813203d7053221c71365d763da26b45
Harishkumar79 <harishsen7976@gmail.com>
2024-10-08 19:19:01 +0530

    commit

---
+++ conditnal statements/demo.java
@@ -0,0 +1,8 @@
+public class demo{
+    public static void main(String[] args) {
+        int a = 63, b = 36;
+        boolean x = (a < b );
+        int y= (a > b ) ? a : b;
+        System.err.println(x + " " + y);
+    }
+}
